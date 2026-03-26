50a408a8256cf86ff7b4d9f08980615abbbdb595
safay <841093111@qq.com>
2026-03-26 15:52:11 +0800

    test:测试提交检验

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { FlowCanvas } from "@/components/flow/canvas-wrapper";
 import { setRequestLocale } from "next-intl/server";
-
+import { Button } from "@/components/ui/button";
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   setRequestLocale(locale);
